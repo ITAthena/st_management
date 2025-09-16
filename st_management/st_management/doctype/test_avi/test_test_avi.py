88e4d2eee532a685fd2d57f015dc80678acf979c
ITAthena <it@rsasurgicalcentre.co.uk>
2025-09-16 12:29:19 +0000

    changes

---
+++ st_management/st_management/doctype/test_avi/test_test_avi.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Bhavesh Maheshwari and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTestAvi(FrappeTestCase):
+	pass
