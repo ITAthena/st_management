3be66731a7492655a228ae3bf133ff0e34b45eb6
ITAthena <it@rsasurgicalcentre.co.uk>
2025-09-20 05:27:29 +0000

    delete the doctype

--- st_management/st_management/doctype/test_avi/test_test_avi.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Bhavesh Maheshwari and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestTestAvi(FrappeTestCase):
-	pass
