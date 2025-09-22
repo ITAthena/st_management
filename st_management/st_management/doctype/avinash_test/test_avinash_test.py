42bc8d15807c4dc23fafd039b792954a99255dad
ITAthena <it@rsasurgicalcentre.co.uk>
2025-09-22 07:49:53 +0000

    changes

---
+++ st_management/st_management/doctype/avinash_test/test_avinash_test.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Bhavesh Maheshwari and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAvinashTest(FrappeTestCase):
+	pass
